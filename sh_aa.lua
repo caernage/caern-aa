3b7877726fa95f060b58892aa41be2ce3c6dadc2
caernage <34951736+caernage@users.noreply.github.com>
2018-03-09 13:01:49 +0100

    Update sh_aa.lua

--- sh_aa.lua
+++ sh_aa.lua
@@ -2,7 +2,7 @@ DarkRP.declareChatCommand{
     command = "aa",
     description = "N/A",
     delay = 1.5,
-	condition = function(ply) if ply:IsAdmin() then return true end end
+    condition = function(ply) if ply:IsAdmin() then return true end end
 }
 
 DarkRP.chatCommandAlias("aa","ac")
